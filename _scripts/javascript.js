89396cfbda19ddfbc04a4290dbe1a7859eb29e7f
4R7HUR <arthursaunders1973@gmail.com>
2014-07-01 06:12:16 +0100

    Create javascript.js

---
+++ _scripts/javascript.js
@@ -0,0 +1,26 @@
+//|||||||||Functions |||||||||||............................
+function a() { }
+function b() { }
+function c() { }
+function d() { }
+function e() { }
+function f() { }
+function g() { }
+function h() { }
+function i() { }
+function j() { }
+
+//|||||||||READY |||||||||||................................
+$(document).ready(function () {
+    // Handler for .ready() called.
+});
+
+//|||||||||LOAD |||||||||||.................................
+$(window).load(function () {
+    // Run when the page is fully loaded including graphics.
+});
+
+//|||||||||BROWSER WIDTH/HEIGHT CHANGE |||||||||||..........
+$(window).resize(function() {
+  // Resize just happened, pixels changed.
+});
